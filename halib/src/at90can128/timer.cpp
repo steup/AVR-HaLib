15a0e9b394d52f4bce3fa9d276574f6f35f3eea3
Karl <Karl@ac1e7a95-1e17-0410-ad1e-f2b06e490645>
2007-11-21 11:36:01 +0000

    git-svn-id: https://svn-eos.cs.uni-magdeburg.de/repos/Projects/AVR/halib/trunk@172 ac1e7a95-1e17-0410-ad1e-f2b06e490645

--- halib/src/at90can128/timer.cpp
+++ halib/src/at90can128/timer.cpp
@@ -19,16 +19,16 @@ bool Timer0::start(uint8_t prescalerId)
 	if (prescalerId > 5)		// Prescaler 32 und 128 nicht unerstützt
 		return false;
 
-//	TCCR0 = prescalerId;		// Prescaler setzen
-//	TIMSK |= _BV(TOIE0);		// Overrun Interrupt Enable
-//	sei();
+	TCCR0A = prescalerId;		// Prescaler setzen
+	TIMSK0 = 1<<TOIE0;		// Overrun Interrupt Enable
+	sei();
 
 	return true;
 }
 
 void Timer0::stop()
 {
-//	TIMSK &= ~_BV(TOIE0);		// Overrun Interrupt Disable
+	TIMSK0 = 0;		// Overrun Interrupt Disable
 }
 
 
@@ -37,16 +37,16 @@ bool Timer1::start(uint8_t prescalerId)
 	if (prescalerId > 5)		// Prescaler 32 und 128 nicht unerstützt
 		return false;
 
-//	TCCR1B = prescalerId;		// Prescaler setzen
-//	TIMSK |= _BV(TOIE1);		// Overrun Interrupt Enable
-//	sei();
+	TCCR1B = prescalerId;		// Prescaler setzen
+	TIMSK1 = 1 << TOIE1;		// Overrun Interrupt Enable
+	sei();
 
 	return true;
 }
 
 void Timer1::stop()
 {
-//	TIMSK &= ~_BV(TOIE1);			// Overrun Interrupt Disable
+	TIMSK1 = 0;			// Overrun Interrupt Disable
 }
 
 
@@ -56,12 +56,12 @@ bool Timer2::start(uint8_t prescalerId)
 		return false;
 
 	// Prescaler 1/x                        -  1  8 64 256 1024 32 128
-//	const uint8_t timer2prescalers [] = { 0, 1, 2, 4,  6,  7, 3,  5};
-//	if (prescalerId > 7)
-//		TCCR2 = 0;
-//	else
-//		TCCR2 = timer2prescalers[prescalerId];
-//	TIMSK |= _BV(TOIE2);				// Overrun Interrupt Enable
+	const uint8_t timer2prescalers [] = { 0, 1, 2, 4,  6,  7, 3,  5};
+	if (prescalerId > 7)
+		TCCR2A = 0;
+	else
+		TCCR2A = timer2prescalers[prescalerId];
+	TIMSK2 = (1 << TOIE2);				// Overrun Interrupt Enable
 	sei();
 
 	return true;
@@ -69,7 +69,7 @@ bool Timer2::start(uint8_t prescalerId)
 
 void Timer2::stop()
 {
-//	TIMSK &= ~_BV(TOIE2);			// Overrun Interrupt Disable
+	TIMSK2 = 0;			// Overrun Interrupt Disable
 }
 
 
