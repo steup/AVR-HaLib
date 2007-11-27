d2ed4aa65dc2239d69db31a7d4eedc0f3d6b86e2
phil <phil@ac1e7a95-1e17-0410-ad1e-f2b06e490645>
2007-11-27 17:44:02 +0000

    git-svn-id: https://svn-eos.cs.uni-magdeburg.de/repos/Projects/AVR/halib/trunk@177 ac1e7a95-1e17-0410-ad1e-f2b06e490645

---
+++ halib/test/usensor.cpp
@@ -0,0 +1,47 @@
+/**
+ *	\brief	Sensor-Test mit Uart
+ *	\author	Philipp Werner
+ *	\date	27.11.2007
+ */
+
+#include "halib/uart.h"
+#include "halib/sensor.h"
+#include "halib/misc.h"
+
+
+
+int main()
+{
+	AnalogSensor s0(0);
+	AnalogSensor s1(1);
+	AnalogSensor s2(2);
+	AnalogSensor s3(3);
+	
+#if defined(__AVR_AT90CAN128__)
+	// Robby-Board
+	DigitalOut power(PORTC, DDRC, 0xff); // Strom fuer die Sensoren, 0 ist an
+#elif defined(__AVR_ATMEGA32__)
+	// Bobby-Board
+	DigitalOut power(PORTD, DDRD, 0x0c);
+#else
+#	error "Board not supported"
+#endif
+	power.replace(0x00);
+
+	Uart<uint16_t, 1000,10> uart;
+	
+	uart << "Reset!\n\r";
+	
+	while(true)
+	{
+			
+		uart << "s0: " << s0.getValue() << "\t"<< s0.getValue()
+				<< "\ts1: " << s1.getValue() << "\t"<< s1.getValue()
+				<< "\ts2: " << s2.getValue() << "\t"<< s2.getValue()
+				<< "\ts3: " << s3.getValue() << "\t"<< s3.getValue();
+		uart.newline();
+		
+		for (volatile uint32_t i = 0; i < 10000; i++)
+			;
+	}
+}
