fbdf59d41c551d2c0a657b704e057811353b4433
kfessel <kfessel@ac1e7a95-1e17-0410-ad1e-f2b06e490645>
2010-10-28 13:44:16 +0000

    BUGFIX: removed recursive make call from loadconfig.mk
    git-svn-id: https://svn-eos.cs.uni-magdeburg.de/repos/Projects/AVR/halib/trunk@538 ac1e7a95-1e17-0410-ad1e-f2b06e490645

--- rules/loadconfig.mk
+++ rules/loadconfig.mk
@@ -4,17 +4,7 @@ include ${RULESDIR}/default.mk
 
 include ${HALIB}/config.mk
 
-$(SHELL echo help)
-
 include $(wildcard config.mk)
 
-%.mk:|%.mk.src
-	cp $@.src $@
-
-config.mk:|${RULESDIR}/config.mk
+config.mk ${HALIB}/config.mk:|${RULESDIR}/config.mk
 	cp ${RULESDIR}/config.mk $@
-
-%.mk:
-	make -C $(dir $@) $(notdir $@)
-
-
