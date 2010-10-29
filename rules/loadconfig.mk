54a3e314d674e7a44620e41f668afa68b149dec4
mschulze <mschulze@ac1e7a95-1e17-0410-ad1e-f2b06e490645>
2010-10-29 11:05:23 +0000

    change include of config.mk to be optional
    git-svn-id: https://svn-eos.cs.uni-magdeburg.de/repos/Projects/AVR/halib/trunk@539 ac1e7a95-1e17-0410-ad1e-f2b06e490645

--- rules/loadconfig.mk
+++ rules/loadconfig.mk
@@ -4,7 +4,7 @@ include ${RULESDIR}/default.mk
 
 include ${HALIB}/config.mk
 
-include $(wildcard config.mk)
+-include $(wildcard config.mk)
 
 config.mk ${HALIB}/config.mk:|${RULESDIR}/config.mk
 	cp ${RULESDIR}/config.mk $@
