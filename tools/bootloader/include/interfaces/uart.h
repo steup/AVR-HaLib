2f47e004cc269e3034f8ef5c0c6815b0045f1fd2
Christoph Steup <steup@ivs.cs.ovgu.de>
2012-03-19 01:08:58 +0100

    Bootloader update

--- tools/bootloader/include/interfaces/uart.h
+++ tools/bootloader/include/interfaces/uart.h
@@ -1,21 +1,28 @@
 #pragma once
 
 #include "avr-halib/avr/uart.h"
+#include <interfaces.h>
 
 namespace avr_halib {
 namespace bootloader {
-namespace interfaces {
+namespace communication {
     struct Uart{
         typedef drivers::Uart::DefaultConfig DefaultConfig;
 
         template<typename Config = DefaultConfig>
-        struct configure{
-            struct NoIntConfig : public Config{
+        struct configure
+        {
+            struct NoIntConfig : public Config
+            {
                 static const bool useInterrupt = false;
             };
 
-           typedef typename 
-               avr_halib::drivers::Uart::configure<NoIntConfig>::type type;
+           struct type : public drivers::Uart::configure< NoIntConfig >::type, interfaces::CommunicationDevice
+            {
+                using drivers::Uart::configure< NoIntConfig >::type::put;
+                using drivers::Uart::configure< NoIntConfig >::type::get;
+
+            };
         };
     };
 }
--- tools/bootloader/src/main.cpp
+++ tools/bootloader/src/main.cpp
@@ -1,32 +1,18 @@
-/*#include <bootloader.h>
+#include <bootloader.h>
 
-struct Config : public avr_halib::bootloader::Bootloader::DefaultConfig{
-    struct InterfaceConfig : public avr_halib::bootloader::Bootloader::DefaultConfig::InterfaceConfig{
+using namespace avr_halib::bootloader;
+
+struct Config : public Bootloader::DefaultConfig
+{
+    struct CommunicationConfig : public Bootloader::DefaultConfig::CommunicationConfig
+    {
         typedef avr_halib::regmaps::local::Uart1 RegMap;
     };
 };
 
-typedef typename avr_halib::bootloader::Bootloader::configure<Config>::type Bootloader;
-
-Bootloader bl;
-
 int main()
 {
+    Bootloader::configure< Config >::type bl;
     bl.run();
-    return 0;
-}*/
-
-#include <avr-halib/avr/flash.h>
-
-typedef avr_halib::drivers::Flash::configure<>::type Flash;
-
-static const char* const content="HalloDu";
-
-int main()
-{
-    Flash::FlashWriter writer;
-    for(uint16_t i=0;i<512;i+=2)
-        writer.write(((uint16_t)content[(i+1)%8]<<8)|content[i%8]);
-
     return 0;
 }
