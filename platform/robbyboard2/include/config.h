85383027113115d68ecccfbbe284c6a7d71d788a
Christoph Steup <steup@ivs.cs.ovgu.de>
2016-01-06 17:19:22 +0100

    Platform: move portmap inclusion out of namespace
    	- portmaps may contain includes
    	- inclusion within namespace may leads to non globally defined types
    	  needed in other files
    	- may arbitrarly break things depending on inclusion order
    	- FIX: add namespaces to portmap and include portmap in global
    	  namespace

--- platform/robbyboard2/include/config.h
+++ platform/robbyboard2/include/config.h
@@ -6,56 +6,55 @@
 #include <avr-halib/common/sleep.h>
 #include <boost/mpl/list.hpp>
 
-namespace platform
+namespace platform {
+namespace robbyboard2 {
+
+using avr_halib::logging::devices::Uart;
+
+typedef avr_halib::config::Frequency< F_CPU > CPUClock;
+typedef avr_halib::config::Frequency< 32768 > RTCClock;
+
+struct LoggingConfig : public Uart::DefaultConfig
 {
-namespace robbyboard2
+		typedef avr_halib::regmaps::local::Uart1 Timer;
+		static const Uart::BaudRateType baudRate = 19200;
+};
+
+typedef Uart::configure<LoggingConfig>::type LoggingDevice;
+
+struct MorpheusConfig : public avr_halib::power::Morpheus::DefaultConfig
 {
-    using avr_halib::logging::devices::Uart;
+		typedef boost::mpl::list< LoggingDevice::SleepSync >::type SyncList;
+};
 
-    typedef avr_halib::config::Frequency< F_CPU > CPUClock;
-    typedef avr_halib::config::Frequency< 32768 > RTCClock;
+typedef avr_halib::power::Morpheus::configure< MorpheusConfig >::type Morpheus;
 
-    struct LoggingConfig : public Uart::DefaultConfig
-    {
-        typedef avr_halib::regmaps::local::Uart1 Timer;
-        static const Uart::BaudRateType baudRate = 19200;
-    };
+struct AsyncTimerBaseConfig
+{
+		typedef avr_halib::regmaps::local::Timer2 Timer;
+		typedef RTCClock TimerFrequency;
+};
 
-    typedef Uart::configure<LoggingConfig>::type LoggingDevice;
+struct Timer0BaseConfig
+{
+		typedef avr_halib::regmaps::local::Timer0 Timer;
+		typedef CPUClock TimerFrequency;
+};
 
-    struct MorpheusConfig : public avr_halib::power::Morpheus::DefaultConfig
-    {
-        typedef boost::mpl::list< LoggingDevice::SleepSync >::type SyncList;
-    };
+struct Timer1BaseConfig
+{
+		typedef avr_halib::regmaps::local::Timer1 Timer;
+		typedef CPUClock TimerFrequency;
+};
 
-    typedef avr_halib::power::Morpheus::configure< MorpheusConfig >::type Morpheus;
+typedef AsyncTimerBaseConfig Timer2BaseConfig;
 
-    struct AsyncTimerBaseConfig
-    {
-        typedef avr_halib::regmaps::local::Timer2 Timer;
-        typedef RTCClock TimerFrequency;
-    };
+struct Timer3BaseConfig
+{
+		typedef avr_halib::regmaps::local::Timer3 Timer;
+		typedef CPUClock TimerFrequency;
+};
 
-    struct Timer0BaseConfig
-    {
-        typedef avr_halib::regmaps::local::Timer0 Timer;
-        typedef CPUClock TimerFrequency;
-    };
+}}
 
-    struct Timer1BaseConfig
-    {
-        typedef avr_halib::regmaps::local::Timer1 Timer;
-        typedef CPUClock TimerFrequency;
-    };
-
-    typedef AsyncTimerBaseConfig Timer2BaseConfig;
-
-    struct Timer3BaseConfig
-    {
-        typedef avr_halib::regmaps::local::Timer3 Timer;
-        typedef CPUClock TimerFrequency;
-    };
-
-    #include <robbyboard2_portmap.h>
-}
-}
+#include <robbyboard2_portmap.h>
